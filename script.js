b22d15b0d5ee0adf14b0e424e0cd02271c3f375f
Suresh Kumar Yadav <suresh23_himcs@rediffmail.com>
2024-10-03 13:40:35 +0530

    Display maps using Leaflet library.

--- script.js
+++ script.js
@@ -14,8 +14,19 @@ const inputElevation = document.querySelector('.form__input--elevation');
 navigator.geolocation.getCurrentPosition(function (currPosition) {
   const { latitude } = currPosition.coords;
   const { longitude } = currPosition.coords;
-  console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  const coords = [latitude, longitude];
+  var map = L.map('map').setView(coords, 13);
+
+  L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
 
 }, function () {
   alert("Could not get your location.")
 });
+
+
