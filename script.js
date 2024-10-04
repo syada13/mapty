4ce26f135d969e531f5dc3e2e53b502a3738cc9a
Suresh Kumar Yadav <suresh23_himcs@rediffmail.com>
2024-10-04 21:14:28 +0530

    Refactor: Implement new methods behaviour.

--- script.js
+++ script.js
@@ -10,16 +10,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
 
 class App {
+  #map;
+  #mapEvent;
   constructor() {
     this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener("change", this._toggleElevationField);
   }
 
   _getPosition() {
     if (navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(this._loadMap, function () {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
         alert("Could not get your location.")
       });
   }
@@ -28,61 +31,50 @@ class App {
     const { latitude } = currPosition.coords;
     const { longitude } = currPosition.coords;
     const coords = [latitude, longitude];
-    map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    map.on('click', function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove("hidden");
-      inputDistance.focus();
-    })
+    }).addTo(this.#map);
+    this.#map.on('click', this._showForm.bind(this))
   }
 
-  _showForm() {
-
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
   }
 
   _toggleElevationField() {
-
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
 
-  _newWorkout() {
+  _newWorkout(e) {
+    e.preventDefault();
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng]).addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup'
+        })
+      )
+      .setPopupContent("Workout")
+      .openPopup();
 
   }
 }
 
-// Create App class object
 const app = new App();
-//app._getPosition();
 
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
 
-  // Clear input fields
-  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
 
-  // Display Marker
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng]).addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup'
-      })
-    )
-    .setPopupContent("Workout")
-    .openPopup();
-});
 
-inputType.addEventListener("change", function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
 
